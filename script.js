b0788a83899d9db226fd68120aa741a79bec9d7a
Anthony <anthonynguyenthanh@gmail.com>
2023-06-26 18:05:42 -0700

    Created playRockPaperScissors function

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 function getComputerChoice() {
     // Math.random(): This function generates a random floating-point number between 0 (inclusive) and 1 (exclusive). (Could be 0.000001, 1.234567, 2.999999, or etc.) It does not accept any arguments. By multiplying the result of Math.random() by 3, we get a random floating-point number between 0 (inclusive) and 3 (exclusive). The generated number can be any decimal value in that range.
-    let randomChoice = Math.floor(Math.random() * 3) + 1;
+    const randomChoice = Math.floor(Math.random() * 3) + 1;
 
     if (randomChoice === 1){
-        return 'Rock';
+        return 'rock';
     }
     else if (randomChoice === 2){
-        return 'Paper';
+        return 'paper';
     }
     else {
-        return 'Scissors';
+        return 'scissors';
     }
 }
 
-// console.log(getComputerChoice());
+function playRockPaperScissors() {
+    let playerSelection = prompt("Choose: rock, paper, or scissors").toLowerCase();
+    const computerSelection = getComputerChoice();
+
+    // Player input check
+    while (playerSelection !== 'rock' || playerSelection !== 'paper' || playerSelection !== 'scissors') {
+        playerSelection = prompt("Choose: rock, paper, or scissors").toLowerCase();
+
+        if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors'){
+            break;
+        }
+    }
+
+    // Ties
+    if ((playerSelection === 'rock' && computerSelection === 'rock') || 
+    (playerSelection === 'paper' && computerSelection === 'paper') || 
+    (playerSelection === 'scissors' && computerSelection === 'scissors')){
+        return "It's a tie!";
+    }
+    // Player Wins
+    else if ((playerSelection === 'rock' && computerSelection === 'scissors') || 
+    (playerSelection === 'scissors' && computerSelection === 'paper') || 
+    (playerSelection === 'paper' && computerSelection === 'rock')){
+        return "You Win! " + (playerSelection[0].toUpperCase() + playerSelection.slice(1)) + " beats " + (computerSelection[0].toUpperCase() + computerSelection.slice(1));
+    }
+    // Player Loses
+    else {
+        return "You Lose! " + (computerSelection[0].toUpperCase() + computerSelection.slice(1)) + " beats " + (playerSelection[0].toUpperCase() + playerSelection.slice(1));
+    }
+}
+
+
+
+console.log(playRockPaperScissors());
