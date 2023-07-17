f934452560d5c44b3bff5abca5b1ba2960b6dcde
Anthony <anthonynguyenthanh@gmail.com>
2023-07-16 17:03:59 -0700

    Added running scores and announces winner

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function to randomly get a computer choice for the game
 function getComputerChoice() {
     // Math.random(): This function generates a random floating-point number between 0 (inclusive) and 1 (exclusive). (Could be 0.000001, 1.234567, 2.999999, or etc.) It does not accept any arguments. By multiplying the result of Math.random() by 3, we get a random floating-point number between 0 (inclusive) and 3 (exclusive). The generated number can be any decimal value in that range.
     const randomChoice = Math.floor(Math.random() * 3) + 1;
@@ -13,12 +14,11 @@ function getComputerChoice() {
     }
 }
 
+// Playing one round of rock paper scissors with the computer
 function playRound(playerSelection) {
     // your code here!
 
-    computerSelection = getComputerChoice();
-
-    // roundResult string
+    const computerSelection = getComputerChoice();
     let roundResult;
 
     // Ties
@@ -33,15 +33,24 @@ function playRound(playerSelection) {
         // return "You Win! " + (playerSelection[0].toUpperCase() + playerSelection.slice(1)) + " beats " + (computerSelection[0].toUpperCase() + computerSelection.slice(1));
 
         roundResult = "You Win! " + (playerSelection[0].toUpperCase() + playerSelection.slice(1)) + " beats " + (computerSelection[0].toUpperCase() + computerSelection.slice(1));
+        playerScore++;
     }
     // Player Loses
     else {
         // return "You Lose! " + (computerSelection[0].toUpperCase() + computerSelection.slice(1)) + " beats " + (playerSelection[0].toUpperCase() + playerSelection.slice(1));
 
         roundResult = "You Lose! " + (computerSelection[0].toUpperCase() + computerSelection.slice(1)) + " beats " + (playerSelection[0].toUpperCase() + playerSelection.slice(1));
+        computerScore++;
     }
     
+    // Text of round result
     result.textContent = roundResult;
+
+    // Update scores
+    score.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
+
+    // Check if game has ended
+    checkGameEnd();
 }
 
 // function game() {
@@ -80,14 +89,34 @@ function playRound(playerSelection) {
 //     }
 // }
 
+// const result = game();
+// console.log(result);
+
+// Violates DRY principle, but this is a small project
+function checkGameEnd() {
+    if (playerScore >= 5) {
+        result.textContent = `Player won! Game over.`;
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+    } else if (computerScore >= 5) {
+        result.textContent = `Computer won! Game over.`;
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+    }
+}
+
+// Initial scores of player and computer
+let playerScore = 0;
+let computerScore = 0;
+
 const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
 
 const result = document.querySelector(".result");
-
-// Replace console.log statements with DOM methods
-// const resultDiv = document.getElementById("result");
+const score = document.querySelector(".score");
 
 rockButton.addEventListener("click", () => {
     playRound("rock"); // Call the playRound function with the correct playerSelection
@@ -100,6 +129,3 @@ paperButton.addEventListener("click", () => {
 scissorsButton.addEventListener("click", () => {
     playRound("scissors"); // Call the playRound function with the correct playerSelection
   });
-
-// const result = game();
-// console.log(result);
